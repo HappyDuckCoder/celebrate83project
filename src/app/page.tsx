613bd24294cc425618b1fc687f7821e31ac6c96b
cyan <onphu2k5@gmail.com>
2025-02-25 22:26:25 +0700

    merge simpleUI to dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,52 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
 import CollaborativeRoom from "@/components/CollaborativeRoom";
-import { getCurrentUser } from "@/lib/action/user.action";
-import { createDocument, getDocuments } from "@/lib/action/room.action";
-import { useRouter } from "next/navigation";
 import RightSidebar from "@/components/SideBar";
+import { createDocument, getDocuments } from "@/lib/action/room.action";
+import { getCurrentUser } from "@/lib/action/user.action";
 import { Lobster, Poppins } from "next/font/google";
-import { SpeakerHigh, SpeakerSlash } from "phosphor-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+// import { SpeakerHigh, SpeakerSlash } from "phosphor-react";
 
 const lobster = Lobster({ weight: "400", subsets: ["latin"] });
 const poppins = Poppins({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
-import RoomControls from "@/components/RoomControl";
 import HelloScreen from "@/components/HelloScreen";
+import RoomControls from "@/components/RoomControl";
 
 const masterRoomId =
-  process.env.NEXT_PUBLIC_MASTER_ROOM_ID || "mWu2Vaq7Mn9QFKeHV7sqU";
+    process.env.NEXT_PUBLIC_MASTER_ROOM_ID || "mWu2Vaq7Mn9QFKeHV7sqU";
 
 const RoomMetadataOfMasterRoom = {
-  title: "Master Room",
-  userEmail: "prto2802@gmail.com",
-  creatorId: "duckilot",
+    title: "Master Room",
+    userEmail: "prto2802@gmail.com",
+    creatorId: "duckilot",
 };
 
 type Room = {
-  type: string;
-  id: string;
-  createdAt: Date;
-  metadata: {
-    title: string;
-    userEmail: string;
-  };
+    type: string;
+    id: string;
+    createdAt: Date;
+    metadata: {
+        title: string;
+        userEmail: string;
+    };
 };
 
 type RoomData = {
-  data: Room[];
+    data: Room[];
 };
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState(true); // Thêm state để kiểm soát loading
-  const [nameGarden, setNameGarden] = useState<string>("");
-  const [idGarden, setIdGarden] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-  const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
+    const [nameGarden, setNameGarden] = useState<string>("");
+    const [idGarden, setIdGarden] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
 
-  const [toggled, setToggled] = useState(true);
+    //   const [toggled, setToggled] = useState(true);
     // const [soundOn, setSoundOn] = useState(null);
     // const [soundOff, setSoundOff] = useState(null);
 
@@ -64,136 +64,138 @@ const Home = () => {
     //         soundOn.play();
     //     }
     // };
-    const router = useRouter();
+    // const router = useRouter();
 
-    const handleClick = () => {
-        setToggled(!toggled);
-        // toggleSound();
-    };
+    // const handleClick = () => {
+    //     setToggled(!toggled);
+    //     // toggleSound();
+    // };
 
-    const handleGetRooms = () => {
-        router.push("/classes");
-    };
+    // const handleGetRooms = () => {
+    //     router.push("/classes");
+    // };
 
-    const handleLogin = () => {
-        console.log("Login button clicked");
-    };
-    
-  // Hiển thị HelloScreen trong 2.5s trước khi vào trang chính
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2500);
+    // const handleLogin = () => {
+    //     console.log("Login button clicked");
+    // };
 
-    return () => clearTimeout(timer);
-  }, []);
+    // Hiển thị HelloScreen trong 2.5s trước khi vào trang chính
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2500);
 
-  const handleCreateRoom = async () => {
-    if (!nameGarden.trim()) {
-      setError("Tên phòng không được để trống!");
-      return;
-    }
-    setLoading(true);
+        return () => clearTimeout(timer);
+    }, []);
 
-    try {
-      const user_ = await getCurrentUser();
-      if (!user_) {
-        setError("Bạn chưa đăng nhập!");
-        setLoading(false);
-        return;
-      }
-
-      const roomsData: RoomData = await getDocuments();
-
-      if (roomsData && Array.isArray(roomsData.data)) {
-        const existingRoom = roomsData.data.find(
-          (roomdata: Room) => roomdata.metadata.userEmail === user_.email
-        );
-
-        if (existingRoom) {
-          setError("Bạn đã có một phòng rồi!");
-          setLoading(false);
-          return;
+    const handleCreateRoom = async () => {
+        if (!nameGarden.trim()) {
+            setError("Tên phòng không được để trống!");
+            return;
         }
-      }
+        setLoading(true);
 
-      const room = await createDocument({
-        title: nameGarden,
-        userId: user_.id,
-        userEmail: user_.email,
-      });
+        try {
+            const user_ = await getCurrentUser();
+            if (!user_) {
+                setError("Bạn chưa đăng nhập!");
+                setLoading(false);
+                return;
+            }
 
-      if (room) {
-        router.push(`/classes/${room.id}`);
-      }
-    } catch (error) {
-      console.error("Lỗi khi tạo phòng:", error);
-      setError("Đã có lỗi xảy ra, vui lòng thử lại!");
-    } finally {
-      setLoading(false);
+            const roomsData: RoomData = await getDocuments();
+
+            if (roomsData && Array.isArray(roomsData.data)) {
+                const existingRoom = roomsData.data.find(
+                    (roomdata: Room) =>
+                        roomdata.metadata.userEmail === user_.email
+                );
+
+                if (existingRoom) {
+                    setError("Bạn đã có một phòng rồi!");
+                    setLoading(false);
+                    return;
+                }
+            }
+
+            const room = await createDocument({
+                title: nameGarden,
+                userId: user_.id,
+                userEmail: user_.email,
+            });
+
+            if (room) {
+                router.push(`/classes/${room.id}`);
+            }
+        } catch (error) {
+            console.error("Lỗi khi tạo phòng:", error);
+            setError("Đã có lỗi xảy ra, vui lòng thử lại!");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleJoinRoom = async () => {
+        if (!idGarden.trim()) {
+            setError("ID phòng không được để trống!");
+            return;
+        }
+
+        router.push(`/classes/${idGarden}`);
+    };
+
+    if (isLoading) {
+        return <HelloScreen />;
     }
-  };
 
-  const handleJoinRoom = async () => {
-    if (!idGarden.trim()) {
-      setError("ID phòng không được để trống!");
-      return;
-    }
+    return (
+        <main className="flex h-full flex-col items-center pt-16">
+            <RoomControls
+                nameGarden={nameGarden}
+                setNameGarden={setNameGarden}
+                idGarden={idGarden}
+                setIdGarden={setIdGarden}
+                handleCreateRoom={handleCreateRoom}
+                handleJoinRoom={handleJoinRoom}
+                loading={loading}
+            />
 
-    router.push(`/classes/${idGarden}`);
-  };
+            {/* Popup lỗi */}
+            {error && (
+                <div className="absolute top-10 right-4 bg-red-500 text-white px-4 py-2 rounded-lg shadow-md">
+                    {error}
+                    <button
+                        onClick={() => setError(null)}
+                        className="ml-2 text-white font-bold"
+                    >
+                        ✖
+                    </button>
+                </div>
+            )}
+            <RightSidebar />
 
-  if (isLoading) {
-    return <HelloScreen />;
-  }
-
-  return (
-    <main className="flex h-full flex-col items-center pt-16">
-      <RoomControls
-        nameGarden={nameGarden}
-        setNameGarden={setNameGarden}
-        idGarden={idGarden}
-        setIdGarden={setIdGarden}
-        handleCreateRoom={handleCreateRoom}
-        handleJoinRoom={handleJoinRoom}
-        loading={loading}
-      />
-
-      {/* Popup lỗi */}
-      {error && (
-        <div className="absolute top-10 right-4 bg-red-500 text-white px-4 py-2 rounded-lg shadow-md">
-          {error}
-          <button
-            onClick={() => setError(null)}
-            className="ml-2 text-white font-bold"
-          >
-            ✖
-          </button>
-        </div>
-      )}
-
-      <div className="text-center flex flex-col items-center relative box-border z-0 w-full pt-8 mt-8">
-        <h1
-                    className={`${lobster.className} font-normal text-5xl text-white my-5 mx-1 drop-shadow-custom rotate-[-3deg] skew-x-[-4deg]`}
+            <div className="text-center flex flex-col items-center relative box-border z-0 w-full pt-8 mt-8">
+                <h1
+                    className={`${lobster.className} font-normal text-5xl text-black my-5 mx-1 drop-shadow-custom rotate-[-3deg] skew-x-[-4deg]`}
                 >
                     Happy Women&apos;s Day
                 </h1>
                 <p
-                    className={`${poppins.className} font-medium text-xl drop-shadow-custom2 text-white`}
+                    className={`${poppins.className} font-medium text-xl drop-shadow-custom2 text-black`}
                 >
                     Chung tay tạo lời chúc <br></br> cùng những bông hoa nhân
                     dịp 8/3
                 </p>
 
-        <div className="mt-4 flex gap-12 justify-center items-center">
-          <CollaborativeRoom
-            roomId={masterRoomId}
-            roomMetadata={RoomMetadataOfMasterRoom}
-          />
-        </div>
-      </div>
-    </main>
-  );
+                <div className="mt-4 flex gap-12 justify-center items-center">
+                    <CollaborativeRoom
+                        roomId={masterRoomId}
+                        roomMetadata={RoomMetadataOfMasterRoom}
+                    />
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Home;
