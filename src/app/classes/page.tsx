e944ef88e2162baad48a579909135f8c2aff38a2
duckilot <prto2802@gmail.com>
2025-03-08 04:26:32 +0700

    xoa clases

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -26,7 +26,7 @@ const ClassesPage = () => {
   // Lấy danh sách phòng
   const fetchRooms = async () => {
     setLoading(true);
-    setIsPermissionGranted(true);
+    setIsPermissionGranted(false);
     setError(null);
     try {
       const response = await getDocuments();
